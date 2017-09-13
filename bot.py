bdf830d04fac10847157dbfb8bcfab6e2dd6baab
Alex Carlin <carlin@ucdavis.edu>
2017-09-12 21:32:14 -0700

    Some changes to structure, and added some comments

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 import time
 from slackclient import SlackClient
 
+BOT_NAME = 'trotsky'
+
 def get_bot_id(bot_name):
     slack_client = SlackClient(os.environ.get('SLACK_TOKEN'))
     api_call = slack_client.api_call("users.list")
@@ -11,27 +13,41 @@ def get_bot_id(bot_name):
             if 'name' in user and user.get('name') == bot_name:
                 return user['id']
 
-BOT_ID = get_bot_id('trotsky')
+BOT_ID = get_bot_id(BOT_NAME)
 TOKEN = os.environ.get('SLACK_TOKEN')
 
-# constants
-AT_BOT = "<@" + BOT_ID + ">"
-
-# instantiate Slack & Twilio clients
 slack_client = SlackClient(TOKEN)
 
 def handle_command(command, channel):
-    response = command or "Generic response" 
+    '''
+    This code only gets called if the bot 'chooses' to respond
+    based on the criteria in `parse_slack_output`
+    
+    command     Original text of the message with the @bot removed 
+    channel     The ID (not name) of the channel the message was posted in
+    '''
+
+    # do the processing steps for the original message. 
+    # Translate? Dyslexize? Something 
+
+    response = command # for now, just say this exact same thing back to us 
     slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            print(output) 
-            if output and 'text' in output and AT_BOT in output['text']:
-                flat_response = output['text'].split(AT_BOT)[1].strip() 
-                return flat_response, output['channel']
+            # Here, we basically control when the bot will respond. 
+            if output and 'text' in output:
+                # If there is output and it has a text field:
+                if BOT_NAME in output['text']:
+                    tmp = '@{}'.format(BOT_NAME) 
+                    text_without_bot_name = output['text'].split(tmp)[1].strip()
+                    # If we choose to respond, we return a tuple: the text of what was said
+                    # to get us to respond, and the channel the message was posted in 
+                    return text_without_bot_name, output['channel'] 
+
+    # If there is no output: 
     return None, None
 
 if __name__ == "__main__":
